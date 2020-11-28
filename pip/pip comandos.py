1704761b1e51c814e5d1acfabeca1fada4ebd70a
higorsantana-omega <omegadosgames02@gmail.com>
2020-11-28 13:32:06 -0300

    pip e venv

---
+++ pip/pip comandos.py
@@ -0,0 +1,16 @@
+# pip3 freeze = Lista todos as bibliotecas instalar
+# cd até o local onde se encontra o arquivo
+# Para criar o requeriments.txt de forma global usa-se o comando
+# pip3 freeze > requeriments.txt
+# Para instalar as dependencias usa-se o comando
+# pip3 install -r requeriments.txt
+# Para instalar uma biblioteca com alguma versão especifica
+# pip install requests=2.19
+
+# para se criar um ambiente virtual
+# Vá até a pasta do projeto e insira o seguinte comando no cmg:
+# python -m venv .venv
+# para entrar no venv
+# .venv\Scripts\activate
+# para sair usa-se
+# deactivate
