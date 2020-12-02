40cac5e3972e2ca39205da2796364a1ad830bfd6
higorsantana-omega <omegadosgames02@gmail.com>
2020-12-02 11:06:51 -0300

    consulta2

--- banco_relacional/consultarEstados.sql
+++ banco_relacional/consultarEstados.sql
@@ -1,2 +1,17 @@
 -- Seleciona todos de estados
 select * from estados
+
+-- Selecionar as duas colunas de `estados`
+-- Para dar nome a uma coluna se usa o as
+select Sigla, nome as 'Nome do estado' from estados
+-- Enquanto regiao for = sul então mostra so da regiao sul
+where regiao = 'sul'
+
+
+-- Selecionar as duas colunas de `estados`
+select nome, regiao from estados
+-- Se a população for maior ou igual 1 10m
+-- irá mostrar
+where populacao >= 10
+-- Ordenar a população de form descrecente
+order by populacao desc
