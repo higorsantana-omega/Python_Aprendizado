14e460e4eabb400429c44c54f867befc9b4fd73f
higorsantana-omega <omegadosgames02@gmail.com>
2020-12-02 11:28:01 -0300

    Delete

---
+++ banco_relacional/excluirEstados.sql
@@ -0,0 +1,11 @@
+-- Deletar da tabela estados
+delete from estados
+-- Que tenha a sigla NV
+where sigla = 'NV'
+
+-- Deletar da tabela estados
+DELETE FROM estados
+-- So se o id for igual ou maior que mil
+WHERE id >= 1000 
+
+select * from estados
