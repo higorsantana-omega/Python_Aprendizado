3cb175ed3ca17522d66ad7e95572e79b23edc3e2
higorsantana-omega <omegadosgames02@gmail.com>
2020-12-05 14:48:07 -0300

    criar banco se  caso nao existir

---
+++ banco_de_dados/criar_banco.py
@@ -0,0 +1,12 @@
+from mysql.connector import connect
+
+# Conectar com o banco de dados
+conexao = connect(
+    host = 'localhost',
+    port = 3306,
+    user = 'root',
+    passwd = 'higors222'
+)
+
+cursor = conexao.cursor()
+cursor.execute('CREATE DATABASE IF NOT EXISTSagenda')
