aa52139ef8d63633630a8c1a69d312734979fe73
higorsantana-omega <omegadosgames02@gmail.com>
2020-12-14 14:31:25 -0300

    incluir varios contatos

---
+++ banco_de_dados/incluir_varios_contatos.py
@@ -0,0 +1,20 @@
+from mysql.connector.errors import ProgrammingError
+from bd import nova_conexao
+
+sql = 'INSERT INTO contatos (nome, tel) VALUES (%s, %s)'
+args = (
+    ('Higor', '963367427'),
+    ('João', '9633632427'),
+    ('Gabriel', '962127427'),
+    ('Alessandro', '96336437'),
+    ('Ronaldo', '963367421')
+)
+
+with nova_conexao() as conexao:
+    try:
+        cursor = conexao.cursor()
+        cursor.executemany(sql, args)
+    except ProgrammingError as e:
+        print(f'Erro: {e.msg}')
+    else:
+        print(f'Foram incluidos {cursor.lastrowid} no registro!')
