0605943bb31d7b8d30c91fb5858ceda4e06eb133
higorsantana-omega <omegadosgames02@gmail.com>
2020-12-14 11:44:41 -0300

    selecionar / incluir

--- banco_de_dados/incluir_varios_contatos.py
+++ banco_de_dados/incluir_varios_contatos.py
@@ -14,6 +14,7 @@ with nova_conexao() as conexao:
     try:
         cursor = conexao.cursor()
         cursor.executemany(sql, args)
+        conexao.commit()
     except ProgrammingError as e:
         print(f'Erro: {e.msg}')
     else:
